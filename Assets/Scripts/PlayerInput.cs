f8c2c0e3252e5df59762b78b7e7eab6d4901b72e
h70129914 <h70129914@gmail.com>
2026-01-16 16:39:11 +0300

    read player input from mouse or keyboard

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public InputType CurrentInputType;
+    public int YMovement { get; set; } 
+
+    void Update()
+    {
+        if (CurrentInputType == InputType.Mouse)
+        {
+            float mouseY = Input.GetAxis("Mouse Y");
+            if (mouseY > 0.1f)
+            {
+                YMovement = 1;
+            }
+            else if (mouseY < -0.1f)
+            {
+                YMovement = -1;
+            }
+            else
+            {
+                YMovement = 0;
+            }
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                YMovement = 1;
+            }
+            else if (Input.GetKey(KeyCode.DownArrow))
+            {
+                YMovement = -1;
+            }
+            else
+            {
+                YMovement = 0;
+            }
+        }
+
+        Debug.Log("YMovement: " + YMovement);
+    }
+}
+
+public enum InputType
+{
+    Keyboard,
+    Mouse,
+}
